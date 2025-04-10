72fdbc212f39984ac70643136b3fe19ab7fc2d06
AndriiIvanov <halalah.han@gmail.com>
2025-04-10 18:02:10 +0300

    changes in generation of different telemetry values

--- src/main/java/org/example/GeoJsonToArrays.java
+++ src/main/java/org/example/GeoJsonToArrays.java
@@ -13,57 +13,122 @@ import java.util.Map;
 import java.util.stream.StreamSupport;
 
 public class GeoJsonToArrays {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) {
         String geoFilePath = "/home/thingsboard/Desktop/generator/src/main/resources/apr10.txt";
         String filePath = "/home/thingsboard/Desktop/thingsboard-demos/src/main/resources/asset_tracking/device_emulators.json";
         try {
+            count(geoFilePath);
 //            parseGeoJson(filePath);
-            updateAssetTrackingJson(geoFilePath, filePath);
+            updateAssetTrackingJson(geoFilePath, filePath, false);
         } catch (IOException e) {
             System.err.println("Error reading the GeoJSON file: " + e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static void updateAssetTrackingJson(String sourcePath, String destinationPath) throws IOException {
-        List<Map<String, List<Double>>> res = parseGeoJson(sourcePath);
+    private static void count(String sourcePath) throws IOException {
+        List<Map<String, List<Double>>> res = parseGeoJson(sourcePath, false);
+        System.out.println("Size: \n" + res.size());
+    }
 
-        ObjectMapper objectMapper = new ObjectMapper();
+    private static void updateAssetTrackingJson(String sourcePath, String destinationPath, boolean printArrays) throws Exception {
+        List<Map<String, List<Double>>> source = parseGeoJson(sourcePath, printArrays);
+
         ArrayNode rootNode = (ArrayNode) objectMapper.readTree(new File(destinationPath));
 
         int count = 0;
-        for (JsonNode jsonNode : rootNode) {
-            ArrayNode telemetryProfiles = (ArrayNode) jsonNode.get("telemetryProfiles");
-            JsonNode firstProfile = StreamSupport.stream(telemetryProfiles.spliterator(), false).filter(node -> "coordinates".equals(node.get("key").asText())).findFirst().get();
-            JsonNode telemetryNode = firstProfile.path("valueStrategy").path("telemetry");
-            if (telemetryNode.isObject() && res.size() > count) {
-                ArrayNode latitudeArray = (ArrayNode) telemetryNode.get("latitude");
-                latitudeArray.removeAll();
-                res.get(count).get("latitude").forEach(latitudeArray::add);
-
-                ArrayNode longitudeArray = (ArrayNode) telemetryNode.get("longitude");
-                longitudeArray.removeAll();
-                res.get(count).get("longitude").forEach(longitudeArray::add);
-            }
+        for (JsonNode fullGeneratorInfoJson : rootNode) {
+            System.out.println(count);
+            ArrayNode telemetryProfiles = (ArrayNode) fullGeneratorInfoJson.get("telemetryProfiles");
+            updateCoordinates(telemetryProfiles, source, count);
+//            updateRssi(telemetryProfiles);
+            updateTemperature(telemetryProfiles);
+            updateBatteryLevel(telemetryProfiles);
             count++;
         }
 
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(destinationPath), rootNode);
     }
 
-    private static List<Map<String, List<Double>>> parseGeoJson(String filePath) throws IOException {
-        List<JsonNode> geometryNode = getJsonNode(filePath);
-        short count = 1;
+    private static void updateCoordinates(ArrayNode telemetryProfiles, List<Map<String, List<Double>>> source, int currentEntityNumber) throws Exception {
+        JsonNode telemetryNode = getTelemetryProfileByKey(telemetryProfiles, "coordinates").path("valueStrategy").path("telemetry");
+        if (telemetryNode.isObject() && source.size() > currentEntityNumber) {
+            ArrayNode latitudeArray = (ArrayNode) telemetryNode.get("latitude");
+            latitudeArray.removeAll();
+            source.get(currentEntityNumber).get("latitude").forEach(latitudeArray::add);
+
+            ArrayNode longitudeArray = (ArrayNode) telemetryNode.get("longitude");
+            longitudeArray.removeAll();
+            source.get(currentEntityNumber).get("longitude").forEach(longitudeArray::add);
+        }
+    }
+
+    private static void updateRssi(ArrayNode telemetryProfiles) throws Exception {
+        JsonNode otherTelemetryNode = getTelemetryProfileByKey(telemetryProfiles, "other").path("valueStrategy").path("telemetry");
+        ArrayNode rssiArray = (ArrayNode) otherTelemetryNode.get("rssi");
+
+        ArrayNode coordinatesArray = (ArrayNode) getTelemetryProfileByKey(telemetryProfiles, "coordinates").path("valueStrategy").path("telemetry").path("latitude");
+        int rssiArraySize = Math.min(coordinatesArray.size(), 100);
+
+        var rssiNewValues = Utility.generateListOfRandomDoublesWithExtremeValues(rssiArraySize, -100, -60, 0);
+        System.out.println("rssi: " + rssiNewValues);
+
+        rssiArray.removeAll();
+        rssiNewValues.forEach(rssiArray::add);
+    }
+
+    private static void updateTemperature(ArrayNode telemetryProfiles) throws Exception {
+        JsonNode otherTelemetryNode = getTelemetryProfileByKey(telemetryProfiles, "other").path("valueStrategy").path("telemetry");
+        ArrayNode temperatureArray = (ArrayNode) otherTelemetryNode.get("temperature");
+
+        ArrayNode coordinatesArray = (ArrayNode) getTelemetryProfileByKey(telemetryProfiles, "coordinates").path("valueStrategy").path("telemetry").path("latitude");
+        int arraySize = coordinatesArray.size() > 80 ? 100 : 20;
+
+        var temperatureNewValues = Utility.generateListOfRandomDoublesWithExtremeValues(arraySize, 30, 40, 0);
+        System.out.println("temperature: " + temperatureNewValues);
+
+        temperatureArray.removeAll();
+        temperatureNewValues.forEach(temperatureArray::add);
+    }
+
+    private static void updateBatteryLevel(ArrayNode telemetryProfiles) throws Exception {
+        JsonNode otherTelemetryNode = getTelemetryProfileByKey(telemetryProfiles, "other").path("valueStrategy").path("telemetry");
+        ArrayNode batteryLevelArray = (ArrayNode) otherTelemetryNode.get("batteryLevel");
+
+        ArrayNode coordinatesArray = (ArrayNode) getTelemetryProfileByKey(telemetryProfiles, "coordinates").path("valueStrategy").path("telemetry").path("latitude");
+        int arraySize = coordinatesArray.size() > 60 ? 100 : 20;
+
+        var batteryLevelNewValues = Utility.generateListOfRandomDoublesWithExtremeValues(arraySize, 15, 90, 2);
+        System.out.println("batteryLevel: " + batteryLevelNewValues);
+
+        batteryLevelArray.removeAll();
+        batteryLevelNewValues.forEach(batteryLevelArray::add);
+    }
+
+    private static JsonNode getTelemetryProfileByKey(ArrayNode telemetryProfiles, String key) throws Exception {
+        return StreamSupport.stream(telemetryProfiles.spliterator(), false)
+                .filter(node -> key.equals(node.get("key").asText()))
+                .findFirst().orElseThrow(() -> new Exception("Telemetry profile with key \"" + key + "\" not found"));
+    }
+
+    private static List<Map<String, List<Double>>> parseGeoJson(String filePath, boolean printArrays) throws IOException {
+        List<JsonNode> geometryNodeList = getListOfGeometries(filePath);
         List<Map<String, List<Double>>> result = new ArrayList<>();
-        for (JsonNode geometry : geometryNode) {
+        short currentTrip = 1;
+
+        for (JsonNode geometryNode : geometryNodeList) {
             List<Double> latitudes = new ArrayList<>();
             List<Double> longitudes = new ArrayList<>();
-            if (geometry.get("type").asText().equals("Point")) {
-                longitudes.add(Utility.round(geometry.get("coordinates").get(0).asDouble(), 10)); // Longitude comes first in GeoJSON
-                latitudes.add(Utility.round(geometry.get("coordinates").get(1).asDouble(), 10)); // Latitude comes second
+            if (geometryNode.get("type").asText().equals("Point")) {
+                longitudes.add(Utility.round(geometryNode.get("coordinates").get(0).asDouble(), 10)); // Longitude comes first in GeoJSON
+                latitudes.add(Utility.round(geometryNode.get("coordinates").get(1).asDouble(), 10)); // Latitude comes second
             }
 
-            if (geometry.get("type").asText().equals("LineString")) {
-                for (JsonNode coordinate : geometry.get("coordinates")) {
+            if (geometryNode.get("type").asText().equals("LineString")) {
+                for (JsonNode coordinate : geometryNode.get("coordinates")) {
                     if (coordinate.isArray() && coordinate.size() >= 2) {
                         longitudes.add(Utility.round(coordinate.get(0).asDouble(), 10)); // Longitude comes first in GeoJSON
                         latitudes.add(Utility.round(coordinate.get(1).asDouble(), 10)); // Latitude comes second
@@ -71,20 +136,24 @@ public class GeoJsonToArrays {
                 }
             }
 
-            System.out.println("Trip " + count++ + ":");
-            System.out.println("Longitudes: \n" + longitudes);
-            System.out.println("Latitudes: \n" + latitudes);
-            System.out.println();
+            if (printArrays) {
+                System.out.println("Trip " + currentTrip++ + ":");
+                System.out.println("Longitudes: \n" + longitudes);
+                System.out.println("Latitudes: \n" + latitudes);
+                System.out.println();
+            }
 
-            result.add(Map.of(
-                    "longitude", longitudes,
-                    "latitude", latitudes
-            ));
+            result.add(
+                    Map.of(
+                            "longitude", longitudes,
+                            "latitude", latitudes
+                    )
+            );
         }
         return result;
     }
 
-    private static List<JsonNode> getJsonNode(String filePath) throws IOException {
+    private static List<JsonNode> getListOfGeometries(String filePath) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode rootNode = objectMapper.readTree(new File(filePath));
         ArrayNode arrayNode = (ArrayNode) rootNode.at("/features");
--- src/main/java/org/example/util/Utility.java
+++ src/main/java/org/example/util/Utility.java
@@ -2,6 +2,8 @@ package org.example.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utility {
     public static double round(double value, int places) {
@@ -18,5 +20,20 @@ public class Utility {
 
     public static double generateRoundedDoubleInRange(int min, int max, int roundingOffset) {
         return round(generateDoubleInRange(min, max), roundingOffset);
+    }
+
+    public static List<Double> generateListOfRandomDoubles(int size, int min, int max, int roundingOffset) {
+        List<Double> array = new ArrayList<>();
+        while (array.size() < size) {
+            array.add(generateRoundedDoubleInRange(min, max, roundingOffset));
+        }
+        return array;
+    }
+
+    public static List<Double> generateListOfRandomDoublesWithExtremeValues(int size, int min, int max, int roundingOffset) {
+        List<Double> array = generateListOfRandomDoubles(size, min, max, roundingOffset);
+        array.add((double) min);
+        array.add((double) max);
+        return array;
     }
 }
